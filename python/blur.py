f6ba0c2f201ff6eea3f9741e451dc0ae1a63964f
Herman Scheele <hermni31122@gmail.com>
2026-04-18 15:51:58 +0200

    blur 3x3 kernel

--- python/blur.py
+++ python/blur.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 from skimage import img_as_float, color
 import imageio.v3 as iio
 
-img = iio.imread("./imgs/me.png")
+img = iio.imread("./imgs/city.jpg")
 img_grey = color.rgb2gray(img)
-img = img_as_float(img_grey).T
+img = img_as_float(img_grey)
 dims = img.shape
 
 
@@ -36,7 +36,7 @@ def kernel(dim:int, g, sig:float) -> np.array:
 
 
 # weighted sum
-def w_sum(x:float, neighs:np.array, k:np.array) -> float:
+def w_sum(neighs:np.array, k:np.array) -> float:
     sx = 0.0
     rows, cols = neighs.shape
     for i in range(rows):
@@ -47,12 +47,16 @@ def w_sum(x:float, neighs:np.array, k:np.array) -> float:
 
 
 # ----------- test
-pxs = np.random.rand(3,3)
-k = kernel(3, g, 0.5)
-print(k)
-print("")
-print(pxs)
-print(w_sum(pxs[1,1], pxs, k))
+# pxs = np.random.rand(3,3)
+# k = kernel(3, g, 0.5)
+
+# print("pixels")
+# print(pxs)
+# print("")
+# print("kernel")
+# print(k)
+# print("")
+
 
 
 
@@ -64,9 +68,13 @@ def blur(px, k_dim:int, sig:float):
     b = np.zeros(dims)
     k = kernel(k_dim, g, sig)
     
+    r = 0
     for i in range(n_rows):
+        r += 1
+        print('row: ', r)
+
         for j in range(n_cols):
-            
+
             neighs = np.zeros((k_dim,k_dim))
             neighs[1][1] = px[i][j]
             # if top
@@ -79,7 +87,7 @@ def blur(px, k_dim:int, sig:float):
                     neighs[2][2] = px[i+1][j+1]
                     
                 # if right corn.
-                elif j == dims[1] - 1:
+                elif j == n_cols - 1:
                     neighs[1][0] = px[i][j-1]
                     neighs[0][0] = px[i+1][j-1]
                     neighs[2][1] = px[i+1][j]
@@ -92,39 +100,76 @@ def blur(px, k_dim:int, sig:float):
                     neighs[1][0] = px[i][j-1]
                     neighs[2][0] = px[i+1][j-1]
 
-                print("i,j = ", (i, j))
-                print(px[i,j])
-                print(neighs)
-                y = w_sum(px[i,j], neighs, k)
-                print(y)
-                print("")
-                b[i,j] = w_sum(px[i,j], neighs, k)
-            
-            # else:
-            #     b[i,j] = px[i,j]
-
             # if floor
+            elif i == n_rows - 1:
+                
                 # if left corn.
-                # if right corn.
-                # else
+                if j == 0:
+                    neighs[0][1] = px[i-1][j]
+                    neighs[0][2] = px[i-1][j+1]
+                    neighs[1][2] = px[i][j+1]
+                
+                # if right corner
+                elif j == n_cols - 1:
+                    neighs[0][0] = px[i-1][j-1]
+                    neighs[0][1] = px[i-1][j]
+                    neighs[1][0] = px[i][j-1]
 
-            # if left wall
-                # if top corn.
-                # if floor corn.
-                # else
+                else:
+                    neighs[0][1] = px[i-1][j]
+                    neighs[0][0] = px[i-1][j-1]
+                    neighs[0][2] = px[i-1][j+1]
+                    neighs[1][2] = px[i][j+1]
+                    neighs[1][0] = px[i][j-1]
+            
+            else:
+                # left wall
+                if j == 0:
+                    neighs[0][1] = px[i-1][j]
+                    neighs[0][2] = px[i-1][j+1]
+                    neighs[1][2] = px[i][j+1]
+                    neighs[2][1] = px[i+1][j]
+                    neighs[2][2] = px[i+1][j+1]
 
-            # if right wall
-                # if top corn.
-                # if floor corn.
-                # else
+                # right wall
+                elif j == n_cols - 1:
+                    neighs[0][1] = px[i-1][j]
+                    neighs[0][0] = px[i-1][j-1]
+                    neighs[1][0] = px[i][j-1]
+                    neighs[2][0] = px[i+1][j-1]
+                    neighs[2][1] = px[i+1][j]
+                
+                # interrior 
+                else:
+                    neighs[0][0] = px[i-1][j-1]
+                    neighs[0][1] = px[i-1][j]
+                    neighs[0][2] = px[i-1][j+1]
+                    neighs[1][0] = px[i][j-1]
+                    
+                    neighs[1][2] = px[i][j+1]
+                    neighs[2][0] = px[i+1][j-1]
+                    neighs[2][1] = px[i+1][j]
+                    neighs[2][2] = px[i+1][j+1]
 
-
-            # else (interior)
-
+            
+            b[i,j] = w_sum(neighs, k)
             
 
     return b
 
+k_dim = 3
+sig = 30.0
+
+k = kernel(k_dim, g, sig)
+b = blur(img, k_dim, sig)
+
+print(k)
+
+plt.imshow(img, cmap='grey')
+plt.show()
+
+plt.imshow(b, cmap='grey')
+plt.show()
 
 # blur_img = blur(img, 3, 0.001)
 # plt.imshow(blur_img)
